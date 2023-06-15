a0099b0ed44526e5c941f3cb75247d52f13cbc0d
noah <marsandnoa@gmail.com>
2023-06-14 22:32:04 -0400

    updating search.py for assignment

--- HW_1_Programming/py/search.py
+++ HW_1_Programming/py/search.py
@@ -12,7 +12,7 @@ by Pacman agents (in searchAgents.py).
 """
 
 import util
-from util import heappush, heappop
+from util import heappush, heappop,PriorityQueue
 class SearchProblem:
     """
     This class outlines the structure of a search problem, but doesn't implement
@@ -68,36 +68,120 @@ def tinyMazeSearch(problem):
     return  [s,s,w,s,w,w,s,w]
 
 def depthFirstSearch(problem):
-    """
-    Search the deepest nodes in the search tree first.
-    Your search algorithm needs to return a list of actions that reaches
-    the goal. Make sure that you implement the graph search version of DFS,
-    which avoids expanding any already visited states. 
-    Otherwise your implementation may run infinitely!
-    To get started, you might want to try some of these simple commands to
-    understand the search problem that is being passed in:
-    print("Start:", problem.getStartState())
-    print("Is the start a goal?", problem.isGoalState(problem.getStartState()))
-    print("Start's successors:", problem.getSuccessors(problem.getStartState()))
-    """
-    """
-    YOUR CODE HERE
-    """
+# a set used to keep track of expanded nodes
+    closed=set()
+# a dict to keep track of the path of nodes to end node
+    expandednode={}
+#set of possible expansion nodes
+    fringe=[]
+    fringe.insert(0, problem.getStartState())
+#list for output of actions
+    actions=[]
 
-    util.raiseNotDefined()
-    
+#continues till no nodes left to expand to
+    while len(fringe)>0:
+#since DFS, the leftmost node is always the first node(nodes are added LIFO) fringe
+      node=fringe.pop(0)
+#exit condition when goal state
+      if (problem.isGoalState(node)):
+#looping through the path to create list of actions
+        while (node!=problem.getStartState()):
+          temptuple=expandednode[node]
+          actions.insert(0,temptuple[1])
+          node=temptuple[0]
+        return actions
+      else:
+#checking to see if node was expanded
+          if (node in closed):
+          	pass
+          else:
+#adding to expanded list
+            closed.add(node)
+#loop for successors
+            for (next_state,action,cost) in problem.getSuccessors(node):
+#add successors to list of possible expansions, adding to front of list for LIFO
+              fringe.insert(0,next_state)
+#dont add any successors that were already expanded, needed for manypaths repeat adding to fringe
+              if (next_state in closed):
+                pass 
+              else:
+#creating path of nodes and actions
+                expandednode[next_state]=(node,action)
 
 def breadthFirstSearch(problem):
-    """
-    YOUR CODE HERE
-    """
-    util.raiseNotDefined()
+    closed=set()
+    expandednode={}
+    fringe=[]
+    fringe.insert(0, problem.getStartState())
+    actions=[]
+    while len(fringe)>0:
+#pulling next node for expansion, since BFS,nodes are added FIFO to fringe
+      node=fringe.pop(0)
+      if (problem.isGoalState(node)):
+        while (node!=problem.getStartState()):
+          temptuple=expandednode[node]
+          actions.insert(0,temptuple[1])
+          node=temptuple[0]
+        return actions
+      else:
+          if (node in closed):
+          	pass
+          else:
+            closed.add(node)
+
+            for (next_state,action,cost) in problem.getSuccessors(node):
+              if (next_state in closed):
+                pass 
+              else:
+                if (fringe.count(next_state)==0):
+                  expandednode[next_state]=(node,action)
+#appending is needed for FIFO
+                fringe.append(next_state)
 
 def uniformCostSearch(problem):
-    """
-    YOUR CODE HERE
-    """
-    util.raiseNotDefined()
+    closed=set()
+    expandednode={}
+#the fringe list was replaced with a priorityQ to pull the lowest cost edge
+    fringe=PriorityQueue()
+#format of item in priorityQ is (node/state,parent node,action from parent to node, cost to current node)
+    fringe.push((problem.getStartState(),'parent','action',0),0)
+    actions=[]
+
+    while not(fringe.isEmpty()):
+#pulling apart next priorityQ item
+#pulling next state from q
+      tempthree=fringe.pop()
+#pulling cost from q
+      currentcost=tempthree[3]
+#checking if this is shortest path to the node
+      if (expandednode.get(tempthree[0],0)==0):
+        expandednode[tempthree[0]]=(tempthree[1],tempthree[2],currentcost)
+      else:
+        if (expandednode.get(tempthree[0])[2]>currentcost):
+          expandednode[tempthree[0]]=(tempthree[1],tempthree[2],currentcost)
+#pulling node
+      node=tempthree[0]
+      if (problem.isGoalState(node)):
+        while (node!=problem.getStartState()):
+          temptuple=expandednode[node]
+          actions.insert(0,temptuple[1])
+          node=temptuple[0]
+        return actions
+      else:
+          if (node in closed):
+          	pass
+          else:
+            closed.add(node)
+
+            for (next_state,action,cost) in problem.getSuccessors(node):
+              if (next_state in closed):
+                pass 
+              else:
+#determining successor cost using the current nodes cost and edge cost
+                truecost=currentcost+cost
+#adding to fringe
+                fringe.update((next_state,node,action,truecost),truecost)
+
 
 def nullHeuristic(state, problem=None):
     """
@@ -107,10 +191,43 @@ def nullHeuristic(state, problem=None):
     return 0
 
 def aStarSearch(problem, heuristic=nullHeuristic):
-    """
-    YOUR CODE HERE
-    """
-    util.raiseNotDefined()
+    closed=set()
+    expandednode={}
+    fringe=PriorityQueue()
+    fringe.push((problem.getStartState(),'parent','action',0,problem),heuristic(problem.getStartState(),problem))
+    actions=[]
+
+    while not(fringe.isEmpty()):
+      tempthree=fringe.pop()
+      currentcost=tempthree[3]
+      if (expandednode.get(tempthree[0],0)==0):
+        expandednode[tempthree[0]]=(tempthree[1],tempthree[2],currentcost)
+      else:
+        if (expandednode.get(tempthree[0])[2]>currentcost):
+          expandednode[tempthree[0]]=(tempthree[1],tempthree[2],currentcost)
+
+      node=tempthree[0]
+      if (problem.isGoalState(node)):
+        while (node!=problem.getStartState()):
+          temptuple=expandednode[node]
+          actions.insert(0,temptuple[1])
+          node=temptuple[0]
+        return actions
+      else:
+          if (node in closed):
+          	pass
+          else:
+            closed.add(node)
+
+            for (next_state,action,cost) in problem.getSuccessors(node):
+              if (next_state in closed):
+                pass 
+              else:
+#calculating actual cost to node, and heuristic+actual cost
+                truecost=currentcost+cost
+                heuristiccost=truecost+heuristic(next_state,problem)
+                fringe.update((next_state,node,action,truecost),heuristiccost)
+
 
 
 # Abbreviations
